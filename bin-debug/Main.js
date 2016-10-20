b1bfc93831663d11ead7c4425722d6440a6e521e
hezenghui <754153731@qq.com>
2016-10-21 00:34:47 +0800

    hahhaha

---
+++ bin-debug/Main.js
@@ -0,0 +1,9 @@
+var Main = (function (_super) {
+    __extends(Main, _super);
+    function Main() {
+        _super.call(this);
+    }
+    var d = __define,c=Main,p=c.prototype;
+    return Main;
+}(egret.DisplayObjectContainer));
+egret.registerClass(Main,'Main');
